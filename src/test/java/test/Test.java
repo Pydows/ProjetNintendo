7d67f6cec008ee107d4fbc890420335e4787a1c3
Frédéric <f.blosse@wanadoo.fr>
2021-05-10 17:05:41 +0200

    creation jeu console dans le main

--- src/test/java/test/Test.java
+++ src/test/java/test/Test.java
@@ -2,6 +2,17 @@ package test;
 
 public class Test {
 
+	Jeu MarioKart = new Jeu("");
+	Jeu JakDaxter = new Jeu("");
+	Jeu AmongUs = new Jeu("");
+	Jeu Sims = new Jeu("");
+	Jeu AnimalCrossing = new Jeu("");
+	
+	Console PC = new Console("");
+	Console PS2 = new Console("");
+	Console Switch = new Console("");
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
